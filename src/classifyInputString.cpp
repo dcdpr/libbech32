c917114a4e0c7bdbbcf3e3aea459f8c7ecb02f22
Daniel X. Pape <dpape@dpape.com>
2018-07-12 16:54:47 -0700

    fix small issues from past pull requests

--- src/classifyInputString.cpp
+++ src/classifyInputString.cpp
@@ -4,29 +4,16 @@ namespace {
 
     InputParam classifyInputStringBase(const std::string & str) {
 
-        if(str.empty())
-            return unknown_param;
-
-        // if exactly 64 chars in length, it is likely a transaction id
-        if(str.length() == 64)
-            return txid_param;
-
-        // if it starts with certain chars, and is of a certain length, it may be a bitcoin address
-        if(str[0] == '1' || str[0] == '3' || str[0] == 'm' || str[0] == 'n' || str[0] == '2')
-            if(str.length() >= 26 && str.length() < 36)
-                return address_param;
-
-        // before testing for various txrefs, get rid of any unknown characters
+        // before testing for various txrefs, get rid of any unknown
+        // characters, ex: dashes, periods
         std::string s = bech32::stripUnknownChars(str);
 
         using namespace txref::limits;
 
-        // may be be txref (with leading HRP)
         if(s.length() == TXREF_STRING_MIN_LENGTH ||
            s.length() == TXREF_STRING_MIN_LENGTH_TESTNET)
             return txref_param;
 
-        // may be be txrefext (with leading HRP)
         if(s.length() == TXREF_EXT_STRING_MIN_LENGTH ||
            s.length() == TXREF_EXT_STRING_MIN_LENGTH_TESTNET)
             return txrefext_param;
@@ -36,17 +23,16 @@ namespace {
 
     InputParam classifyInputStringMissingHRP(const std::string & str) {
 
-        // before testing for various txrefs, get rid of any unknown characters
+        // before testing for various txrefs, get rid of any unknown
+        // characters, ex: dashes, periods
         std::string s = bech32::stripUnknownChars(str);
 
         using namespace txref::limits;
 
-        // may be be txref (without leading HRP)
         if(s.length() == TXREF_STRING_NO_HRP_MIN_LENGTH ||
            s.length() == TXREF_STRING_NO_HRP_MIN_LENGTH_TESTNET)
             return txref_param;
 
-        // may be be txrefext (without leading HRP)
         if(s.length() == TXREF_EXT_STRING_NO_HRP_MIN_LENGTH ||
            s.length() == TXREF_EXT_STRING_NO_HRP_MIN_LENGTH_TESTNET)
             return txrefext_param;
@@ -58,7 +44,22 @@ namespace {
 
 InputParam classifyInputString(const std::string & str) {
 
+    if(str.empty())
+        return unknown_param;
+
+    // if exactly 64 chars in length, it is likely a transaction id
+    if(str.length() == 64)
+        return txid_param;
+
+    // if it starts with certain chars, and is of a certain length, it may be a bitcoin address
+    if(str[0] == '1' || str[0] == '3' || str[0] == 'm' || str[0] == 'n' || str[0] == '2')
+        if(str.length() >= 26 && str.length() < 36)
+            return address_param;
+
+    // check if it could be a standard txref or txrefext
     InputParam baseResult = classifyInputStringBase(str);
+
+    // check if it could be a truncated txref or txrefext (missing the HRP)
     InputParam missingResult = classifyInputStringMissingHRP(str);
 
     // if one result is 'unknown' and the other isn't, then return the good one
@@ -67,7 +68,7 @@ InputParam classifyInputString(const std::string & str) {
     if(baseResult == unknown_param && missingResult != unknown_param)
         return missingResult;
 
-    // special case: if baseResult is 'txref' and the other is 'txrefext' then we need to dig
+    // special case: if baseResult is 'txref' and missingResult is 'txrefext' then we need to dig
     // deeper as mainnet min txref with HRP is same length as mainnet min txrefext without HRP
     if (baseResult == txref_param && missingResult == txrefext_param) {
         if (str[0] == 't' && str[1] == 'x' && str[2] == '1')
@@ -76,6 +77,7 @@ InputParam classifyInputString(const std::string & str) {
             return txrefext_param;
     }
 
-    // otherwise, just return (should be unknown)
+    // otherwise, just return
+    assert(baseResult == unknown_param);
     return baseResult;
 }
